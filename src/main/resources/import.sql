579cdd1351a238ce3491dc9a26af20e67bf10dec
Julio <jcpumahuacre@gmail.com>
2025-07-04 00:09:20 -0500

    add import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO client (name, email) VALUES ('Julio', 'julio@email.com');
+INSERT INTO client (name, email) VALUES ('Alberto', 'Alberto@email.com');
+INSERT INTO client (name, email) VALUES ('Juan', 'Juan@email.com');
